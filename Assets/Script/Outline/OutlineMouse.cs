4a8378d204f5274b40be3092049d3e9878c85085
KI22766 <kokorowatareyumewatare@yahoo.co.jp>
2023-05-26 13:16:36 +0900

    アウトラインの修正

--- Assets/Script/Outline/OutlineMouse.cs
+++ Assets/Script/Outline/OutlineMouse.cs
@@ -6,33 +6,59 @@ public class OutlineMouse : MonoBehaviour
 {
     private bool isMouseOver = false;
     private Outline outline;
-
+    private Vector3 ConPosPre = Vector3.zero;
+    Ray ray;
+    GameObject gameObject2 = null;
     void Start()
     {
         outline = GetComponent<Outline>();
         outline.enabled = false;
     }
 
-    void OnMouseEnter()
-    {
-        isMouseOver = true;
-    }
+    //void OnMouseEnter()
+    //{
+    //    isMouseOver = true;
+    //}
 
-    void OnMouseExit()
-    {
-        isMouseOver = false;
-        outline.enabled = false;
-    }
+    //void OnMouseExit()
+    //{
+    //    isMouseOver = false;
+    //    outline.enabled = false;
+    //}
 
     void Update()
     {
-        if (isMouseOver)
+        Vector3 _imgRect = TestUIRay.instance._imgRect.position;
+       
+        ray = Camera.main.ScreenPointToRay(TestUIRay.instance.GetUIScreenPos(TestUIRay.instance._imgRect));
+        
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
         {
-            outline.enabled = true;
+            gameObject2 = hit.collider.gameObject;
+            if (gameObject2.CompareTag("Cube"))
+            {
+                outline.enabled = true;
+            }
+            else
+            {
+                outline.enabled = false;
+            }
         }
         else
         {
             outline.enabled = false;
         }
+
+
+        //if (isMouseOver)
+        //{
+        //    outline.enabled = true;
+        //}
+        //else
+        //{
+        //    outline.enabled = false;
+        //}
     }
 }
